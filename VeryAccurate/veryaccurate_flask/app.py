1b596e1d15c944aab8ecfe01de712be0476c3dab
Craneagehu <hu85650695@126.com>
2019-07-25 16:51:48 +0800

    。。

--- VeryAccurate/veryaccurate_flask/app.py
+++ VeryAccurate/veryaccurate_flask/app.py
@@ -3,11 +3,30 @@ import subprocess
 import time
 from queue import Queue
 from veryaccurate import VeryAccurate
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from gevent.pywsgi import WSGIServer
 
 
 app = Flask(__name__)
+
+
+@app.route('/VeryAccurate2/<flight_num>&<date>',methods=['GET', 'POST'])
+def VeryAccurate2(flight_num,date):
+    if request.method == 'GET':
+        flight_num = flight_num.split('=')[1].upper().strip()
+        date = date.split('=')[1].strip()
+        data =  VeryAccurate(flight_num,date).MyThread()
+
+        if data[0] != "暂无数据":
+            resp = jsonify({'result':data})
+            resp.headers['Access-Control-Allow-Origin'] = '*'
+            return resp
+
+        else:
+            resp = jsonify(data)
+            resp.headers['Access-Control-Allow-Origin'] = '*'
+            return resp
+
 
 
 #
@@ -23,24 +42,9 @@ app = Flask(__name__)
 #             # print(f"耗时: {t2-t1}")
 #             return data
 
-@app.route('/VeryAccurate2/<flight_num>&<date>',methods=['GET', 'POST'])
-def VeryAccurate2(flight_num,date):
-    if request.method == 'GET':
-        flight_num = flight_num.split('=')[1].upper().strip()
-        date = date.split('=')[1]
-        data =  VeryAccurate(flight_num,date).MyThread()
-        if data[0] != "暂无数据":
-            data = {"result": data}
-            return json.dumps(data,ensure_ascii=False)
-
-        else:
-            data = {"result": data[0]}
-            return json.dumps(data, ensure_ascii=False)
-
-
 
 
 if __name__ == '__main__':
     app.config["JSON_AS_ASCII"] = False
-    #app.run(debug=True,host= '0.0.0.0',port=5000)
-    WSGIServer(('0.0.0.0', 5000), app).serve_forever()
+    app.run(debug=True,host= '0.0.0.0',port=5000)
+    # WSGIServer(('0.0.0.0', 5000), app).serve_forever()
--- VeryAccurate/veryaccurate_flask/veryaccurate.py
+++ VeryAccurate/veryaccurate_flask/veryaccurate.py
@@ -2,25 +2,23 @@
 import json
 import re
 import time
-import threading
-from urllib.parse import urljoin
-from urllib.request import urlretrieve
-
 import pymysql
-from pyquery import PyQuery as pq
+import threading
 import requests
 from PIL import Image
-from bs4 import BeautifulSoup
+from queue import Queue
 from lxml import etree
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 from fake_useragent import UserAgent
 from pytesseract import pytesseract
-from requests.cookies import RequestsCookieJar
+
 
 
 class VeryAccurate(object):
     def __init__(self,flightnum,date):
         self.Info = []
-        self.flightnum = flightnum.upper()
+        self.flightnum = flightnum
         self.date = date
 
         self.start_url = f"http://www.variflight.com/flight/fnum/{self.flightnum}.html?AE71649A58c77&fdate={self.date}"
@@ -36,6 +34,7 @@ class VeryAccurate(object):
 
         }
         self.lock = threading.Lock()
+        self.q = Queue()
         self.conn = pymysql.connect(host='localhost', user='root', password='admin', database='data_query', port=3306,charset='utf8')
         self.cur = self.conn.cursor()
 
@@ -53,8 +52,157 @@ class VeryAccurate(object):
         code = pytesseract.image_to_string(image)
         return code
 
-    #数据库持久化
-    def save2Mysql(self,data):
+
+
+    def parse(self):
+        self.lock.acquire()
+        try:
+            if not self.q.empty():
+                self.q.get()
+                start_response = requests.get(self.start_url,headers=self.headers)
+                start_response.encoding = "utf-8"
+                e = etree.HTML(start_response.text)
+                seach_result = e.xpath('//*[@class="searchlist_innerli"]')
+
+                if seach_result:
+                    name = e.xpath('//*[@class="tit"]/h1/@title')[0].strip()
+                    log = "{0}航班存在信息...".format(name)
+                    print(log)
+                    mylist = e.xpath('//*[@id="list"]/li')
+                    for selector in mylist:
+                        # 获取cookie值
+                        cookie = {}
+                        cookies = start_response.headers['Set-Cookie']
+                        salt = re.findall('salt=(.*?);', cookies)[0]
+                        cookie['Cookie'] = f'salt={salt}'
+
+                        # 航班信息
+                        flight_info = selector.xpath('div[@class="li_com"]/span[1]/b/a//text()')
+                        flight_company = flight_info[0]
+                        flight_num = flight_info[1]
+
+                        # 计划起飞
+                        FlightDeptimePlan = selector.xpath('div[@class="li_com"]/span[2]/@dplan')[0]
+
+                        # 实际起飞
+                        FlightDeptime_img_link = selector.xpath('div[@class="li_com"]/span[6]/img/@src')
+
+                        #计划到达
+                        FlightArrtimePlan = selector.xpath('div[@class="li_com"]/span[5]/@aplan')[0]
+
+                        # 实际到达
+                        FlightArrtime_img_link = selector.xpath('div[@class="li_com"]/span[3]/img/@src')
+
+                        if FlightDeptime_img_link and FlightArrtime_img_link:
+                            FlightDeptime_img_link = urljoin(start_response.url, FlightDeptime_img_link[0])
+                            time1 = self.parse_time(FlightDeptime_img_link,cookie)
+                            _time1 = time1.split(":")[0] if time1.split(":")[0][0] else time1.split(":")[0].replace('0', '')
+
+                            FlightArrtime_img_link = urljoin(start_response.url, FlightArrtime_img_link[0])
+                            time2 = self.parse_time(FlightArrtime_img_link,cookie)
+                            _time2 = time2.split(":")[0] if time2.split(":")[0][0] else time2.split(":")[0].replace('0','')
+
+                            if _time1 < _time2:
+                                FlightDeptime = time1
+                                FlightArrtime = time2
+
+                            elif _time1 > _time2:
+                                FlightDeptime = time2
+                                FlightArrtime = time1
+
+
+                        elif FlightDeptime_img_link and not FlightArrtime_img_link:
+                            FlightDeptime_img_link = urljoin(start_response.url, FlightDeptime_img_link[0])
+                            FlightDeptime = self.parse_time(FlightDeptime_img_link,cookie)
+                            FlightArrtime = ''
+
+                        elif FlightArrtime_img_link and not FlightDeptime_img_link:
+                            FlightArrtime_img_link = urljoin(start_response.url, FlightArrtime_img_link[0])
+                            FlightDeptime = self.parse_time(FlightArrtime_img_link,cookie)
+                            FlightArrtime = ''
+
+                        else:
+                            FlightDeptime = ''
+                            FlightArrtime = ''
+
+
+                        detailpage_link = selector.xpath('a[@class="searchlist_innerli"]/@href')[0]
+                        full_url = urljoin(start_response.url,detailpage_link)
+
+                        response = requests.get(full_url,headers = self.headers)
+                        response.encoding = "utf-8"
+
+                        soup = BeautifulSoup(response.text,'lxml')
+
+                        item = {}
+
+                        item["FlightNo"] = self.flightnum
+
+                        item["FlightCompany"] = flight_company
+
+                        info_dep = soup.select('div[class="f_title f_title_a"] h2')[0].text.strip()
+
+                        # 出发地
+                        item["FlightDepAirport"] = re.findall(r'[\u4e00-\u9fa5]+',info_dep)[0]
+
+                        # 登机口
+                        item["FlightHTerminal"] = re.findall(r'T\d?\w',info_dep)[0] if re.findall(r'T\d?\w',info_dep) else ''
+
+                        #计划起飞
+                        item["FlightDeptimePlan"] = FlightDeptimePlan
+
+                        # 实际起飞
+                        item["FlightDeptime"] = FlightDeptime
+
+                        info_arr = soup.select('div[class="f_title f_title_c"] h2')[0].text.strip()
+
+                        #到达城市
+                        item["FlightArrAirport"] = re.findall(r'[\u4e00-\u9fa5]+',info_arr)[0]
+
+                        # 出机口
+                        item["FlightTerminal"] = re.findall(r'T\d?\w',info_arr)[0] if re.findall(r'T\d?\w',info_arr) else ''
+
+                        #计划到达
+                        item['FlightArrtimePlan'] = FlightArrtimePlan
+
+                        #实际到达
+                        item["FlightArrtime"] = FlightArrtime
+
+                        #机型
+                        item["generic"] = soup.select('li[class="mileage"]')[0].text[3:].strip()
+
+                        #机龄
+                        item["FlightYear"] = soup.select('li[class="time"] span')[0].text
+
+                        #准点率
+                        OntimeRate_link = selector.xpath('div[@class="li_com"]/span[8]/img/@src')[0]
+                        OntimeRate_link = urljoin(response.url,OntimeRate_link)
+                        item["OntimeRate"] = self.parse_time(OntimeRate_link, cookie)
+
+                        #里程
+                        item["distance"] = soup.select('span[class="ti"]')[0].text
+
+                        #飞行时间
+                        item["FlightDuration"] = soup.select('span[class="ti"]')[1].text
+
+                        #飞机状态
+                        item["FlightState"] = soup.select('div[class="state"]')[0].text.strip()
+                        print(item)
+                        self.Info.append(item)
+                        self.save2Mysql(item)
+
+                else:
+                    name = e.xpath('//*[@id="byNumInput"]/@value')[0]
+                    log = "{0}航班不存在信息".format(name)
+                    print(log)
+
+        except Exception as e:
+            print(f'程序出现异常!!!: {e}')
+        self.lock.release()
+
+        # 数据库持久化
+
+    def save2Mysql(self, data):
         keys = ','.join(data.keys())
         values = ','.join(['%s'] * len(data))
 
@@ -62,156 +210,10 @@ class VeryAccurate(object):
         self.cur.execute(insert_sql, tuple(data.values()))
         self.conn.commit()
 
-    def parse(self):
-        self.lock.acquire()
-        try:
-            start_response = requests.get(self.start_url,headers=self.headers)
-            start_response.encoding = "utf-8"
-            e = etree.HTML(start_response.text)
-            seach_result = e.xpath('//*[@class="searchlist_innerli"]')
-
-
-            if seach_result:
-                name = e.xpath('//*[@class="tit"]/h1/@title')[0].strip()
-                log = "{0}航班存在信息...".format(name)
-                print(log)
-                mylist = e.xpath('//*[@id="list"]/li')
-                for selector in mylist:
-                    # 获取cookie值
-                    cookie = {}
-                    cookies = start_response.headers['Set-Cookie']
-                    salt = re.findall('salt=(.*?);', cookies)[0]
-                    cookie['Cookie'] = f'salt={salt}'
-
-                    # 航班信息
-                    flight_info = selector.xpath('div[@class="li_com"]/span[1]/b/a//text()')
-                    flight_company = flight_info[0]
-                    flight_num = flight_info[1]
-
-                    # 计划起飞
-                    FlightDeptimePlan = selector.xpath('div[@class="li_com"]/span[2]/@dplan')[0]
-
-                    # 实际起飞
-                    FlightDeptime_img_link = selector.xpath('div[@class="li_com"]/span[6]/img/@src')[0]
-
-                    #计划到达
-                    FlightArrtimePlan = selector.xpath('div[@class="li_com"]/span[5]/@aplan')[0]
-
-                    # 实际到达
-                    FlightArrtime_img_link = selector.xpath('div[@class="li_com"]/span[3]/img/@src')[0]
-
-                    if FlightDeptime_img_link and FlightArrtime_img_link:
-                        FlightDeptime_img_link = urljoin(start_response.url, FlightDeptime_img_link)
-                        time1 = self.parse_time(FlightDeptime_img_link,cookie)
-                        _time1 = time1.split(":")[0] if time1.split(":")[0][0] else time1.split(":")[0].replace('0', '')
-
-                        FlightArrtime_img_link = urljoin(start_response.url, FlightArrtime_img_link)
-                        time2 = self.parse_time(FlightArrtime_img_link,cookie)
-                        _time2 = time2.split(":")[0] if time2.split(":")[0][0] else time2.split(":")[0].replace('0','')
-
-                        if _time1 < _time2:
-                            FlightDeptime = time1
-                            FlightArrtime = time2
-
-                        elif _time1 > _time2:
-                            FlightDeptime = time2
-                            FlightArrtime = time1
-
-
-                    elif FlightDeptime_img_link and not FlightArrtime_img_link:
-                        FlightDeptime_img_link = urljoin(start_response.url, FlightDeptime_img_link)
-                        FlightDeptime = self.parse_time(FlightDeptime_img_link,cookie)
-                        FlightArrtime = '暂无信息'
-
-                    elif FlightArrtime_img_link and not FlightDeptime_img_link:
-                        FlightArrtime_img_link = urljoin(start_response.url, FlightArrtime_img_link)
-                        FlightDeptime = self.parse_time(FlightArrtime_img_link,cookie)
-                        FlightArrtime = '暂无信息'
-
-                    else:
-                        FlightDeptime = '暂无信息'
-                        FlightArrtime = '暂无信息'
-
-
-                    detailpage_link = selector.xpath('a[@class="searchlist_innerli"]/@href')[0]
-                    full_url = urljoin(start_response.url,detailpage_link)
-
-                    response = requests.get(full_url,headers = self.headers)
-                    response.encoding = "utf-8"
-
-                    soup = BeautifulSoup(response.text,'lxml')
-
-                    item = {}
-
-                    item["FlightNo"] = self.flightnum
-
-                    item["FlightCompany"] = flight_company
-
-                    info_dep = soup.select('div[class="f_title f_title_a"] h2')[0].text.strip()
-
-                    # 出发地
-                    item["FlightDepAirport"] = re.findall(r'[\u4e00-\u9fa5]+',info_dep)[0]
-
-                    # 登机口
-                    item["FlightHTerminal"] = re.findall(r'T\d?\w',info_dep)[0] if re.findall(r'T\d?\w',info_dep) else ''
-
-                    #计划起飞
-                    item["FlightDeptimePlan"] = FlightDeptimePlan
-
-                    # 实际起飞
-                    item["FlightDeptime"] = FlightDeptime
-
-                    info_arr = soup.select('div[class="f_title f_title_c"] h2')[0].text.strip()
-
-                    #到达城市
-                    item["FlightArrAirport"] = re.findall(r'[\u4e00-\u9fa5]+',info_arr)[0]
-
-                    # 出机口
-                    item["FlightTerminal"] = re.findall(r'T\d?\w',info_arr)[0] if re.findall(r'T\d?\w',info_arr) else ''
-
-                    #计划到达
-                    item['FlightArrtimePlan'] = FlightArrtimePlan
-
-                    #实际到达
-                    item["FlightArrtime"] = FlightArrtime
-
-                    #机型
-                    item["generic"] = soup.select('li[class="mileage"]')[0].text[3:].strip()
-
-                    #机龄
-                    item["FlightYear"] = soup.select('li[class="time"] span')[0].text
-
-                    #准点率
-                    OntimeRate_link = selector.xpath('div[@class="li_com"]/span[8]/img/@src')[0]
-                    OntimeRate_link = urljoin(response.url,OntimeRate_link)
-                    item["OntimeRate"] = self.parse_time(OntimeRate_link, cookie)
-
-                    #里程
-                    item["distance"] = soup.select('span[class="ti"]')[0].text
-
-                    #飞行时间
-                    item["FlightDuration"] = soup.select('span[class="ti"]')[1].text
-
-                    #飞机状态
-                    item["FlightState"] = soup.select('div[class="state"]')[0].text.strip()
-                    print(item)
-                    self.Info.append(item)
-                    self.save2Mysql(item)
-
-            else:
-                name = e.xpath('//*[@id="byNumInput"]/@value')[0]
-                log = "{0}航班不存在信息".format(name)
-                print(log)
-
-        except Exception as e:
-            print(f'程序出现异常!!!: {e}')
-
-        self.lock.release()
-
-
     def MyThread(self):
         thread_list = []
-        for i in range(1):
+        self.q.put(self.flightnum)
+        for i in range(6):
             thread = threading.Thread(target=self.parse)
             thread_list.append(thread)
             thread.start()
@@ -223,6 +225,6 @@ class VeryAccurate(object):
 
 if __name__ == "__main__":
     flight_num = "SC1155"
-    date = '20190723'
+    date = '20190725'
     veryaccurate = VeryAccurate(flight_num,date)
     veryaccurate.MyThread()
